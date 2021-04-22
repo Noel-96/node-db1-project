3fc740c6fbbb346bd37179425dd35ebb6c64fdbc
Noel-96 <noelorion69@gmail.com>
2021-04-22 14:49:35 +0100

    mvp done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,62 @@
-exports.checkAccountPayload = (req, res, next) => {
+const accounts = require("../accounts/accounts-model")
+
+exports.checkAccountPayload  = () => {
   // DO YOUR MAGIC
+  return (req, res, next) => {
+    if(!req.body.name || !req.body.budget){
+      return res.status(400).json({
+        message: "name and budget are required"
+      })
+    } else if(typeof req.body.name !== "string") {
+        return res.status(400).json({
+        message: "name of account must be a string"
+        })
+    } else if(typeof req.body.budget !== "number") {
+        return res.status(400).json({
+          message: "budget of account must be a number"
+        })
+    } else if(req.body.budget < 0 ) {
+      return res.status(400).json({
+        message: "budget of account can not be negative"
+      })
+    }
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique  = () => {
   // DO YOUR MAGIC
+  return (req, res, next) => {
+    accounts.getAll()
+      .then(accounts => {
+        if(accounts.find({
+          username: req.body.name.trim()
+        })) {
+          return res.status(400).json({
+          message: "that name is taken"
+          })
+        }
+
+        next()
+      })
+    }
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId  = () => {
   // DO YOUR MAGIC
+  return (req, res, next) => {
+    accounts.getById(req.params.id)
+    .then(account => {
+      if(account) {
+        req.account = account
+        next()
+    } else {
+        res.status(404).json({
+          message: "account not found"
+        })
+      }
+   })
+
+    .catch(next)
+  }
 }
